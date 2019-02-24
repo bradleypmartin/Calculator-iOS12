3f12e0d75e5422305aaeba5fcf191d286653b3e6
bradleypmartin <brad.martin@ibotta.com>
2019-02-23 20:21:57 -0700

    in the middle of completing first basic calls

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,20 +12,33 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    private var isFinishedTypingNumber = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        displayLabel.text = "0"
+        isFinishedTypingNumber = true
+        
+        let number = Double(displayLabel.text!)!
+        
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+        }
+        
     }
 
 }
-
